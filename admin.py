d8b70ed3a72f7fb61f0bdd907e787756a75d228a
LGhomi <leilaghomi73@gmail.com>
2020-11-18 21:25:26 +0330

    admin and read courses changed

--- admin.py
+++ admin.py
@@ -1,94 +1,54 @@
-import csv
-
 from user import User
-from course import Course
-import logging
-from write_courses import write_courses
-
-
-# from student_class import Student
+import dtabases.courses.read_corses as db
 
 
 # logging.basicConfig(filename="msg.log", filemode='a', level=logging.DEBUG, format='%(asctime)s - %(message)s')
 
 class Admin(User):
-    students_list = r'D:\maktabsharif\python_project\university-platform\dtabases\users\users_db.csv'
-
-    def __init__(self, username, password, user_id, firstname, lastname, user_type, field_name, field_code):
-        super().__init__(username, password, user_id, firstname, lastname, user_type, field_name, field_code)
-
     def menu_message(self):
         """
         show menu to admin
         :return: nothing
         """
-        super().menu_message()
-        print('3.Define a course\n4.Show students\n5.Choose student\n6.Check student courses(Pass or Reject)')
+        print(
+            'Please Select an option from the following menu:\n1.logout\n2.Define a course\n3.Show students\n4.Choose student\n6.Check student courses(Pass or Reject)')
 
     def define_course(self, name, units, total_quantity, teacher_name, course_code, field_code):
         """
-        define a course with method's attributes
+        define a course with write method
         :return: nothing
         """
 
-        # reference to Course class
-        # course = Course(name, units, total_quantity, teacher_name, course_code, field_code)
-        # write_courses(
-        #     {'name': course.name, 'units': course.units, 'total_quantity': course.total_quantity,
-        #      'teacher_name': course.teacher_name, 'course_code': course.course_code,
-        #      'field_code': course.field_code})
+        db.write_db(name, units, total_quantity, teacher_name, course_code, field_code)
 
-        self.name = name
-        self.units = units
-        self.total_quantity = total_quantity
-        self.remaining_quantity = total_quantity
-        self.teacher_name = teacher_name
-        self.course_code = course_code
-        self.field_code = field_code
-        # write_courses(self.name, units, total_quantity, teacher_name, course_code, field_name, field_code)
-        write_courses(
-            {'name': self.name, 'units': self.units, 'total_quantity': self.total_quantity,
-             'teacher_name': self.teacher_name, 'course_code': self.course_code,
-             'field_code': self.field_code})
-        # log add a lesson
-        # logging.info("a lesson is added")
+    def show_students(self, students_list):
+        """
+        Show students list whit specifications
+        :param students_list: list of students in all fields
+        :return: nothing
+        """
+        for user in students_list:
+            if user.field_code == self.field_code:
+                print(user)
 
-    def show_students(self):
-        # Show students list whit specifications
-        with open(self.students_list, 'r') as stu_list:
-            reader = csv.DictReader(stu_list)
-            for row in reader:
-                print(row['username'], row['password'], row['firstname'], row['lastname'], row['user_id'],
-                      row['user_type'], row['field_name'], row['field_code'])
+    def choose_student(self, user_id, students_list):
+        """
+        Choose student with id and then show selected units and lessons
+        :param user_id: id for selected user that admin want to see lessons
+        :param students_list: list of students in all fields
+        :return: nothing
+        """
+        for user in students_list:
+            if user.user_id == user_id:
+                for lesson in user.chosen_courses:
+                    print(lesson)
 
-    def choose_student(self, user_id):
-        # choose student with id and then show selected units and lessons
-        self.user_id = user_id
-        with open(self.students_list, 'r') as stu_list:
-            reader = csv.DictReader(stu_list)
-            for row in reader:
-                if row['user_id']==user_id:
-                    pass
-                    # show lessons
-                return True
-            else:
-                return False
-
-    # check student courses and pass or reject
-    def check_student_course(self, status):
-        self.status = status
-        if self.status is True:
-            logging.info("Passed")
-            print("Check Student Course : Passed")
-            # change a variable in student class
-        else:
-            logging.info("Rejected")
-            print("Check Student Course : Rejected")
-        return status
-
-
-# a = Admin('35111113', '4210ab', '35111113', 'Nastaran', 'Magham', '1', 'Biomedical engineering', '33')
-# a.menu_message()
-# # a.define_course('Farsiiiii', '3', '7', 'Afrasiab', '115', '0')
-# a.show_students()
-# a.choose_student('95991311')
+    def check_student_course(self, student, status):
+        """
+        Check student courses and pass or reject
+        :param student: object of Student class
+        :param status: admin status for pass(True) or reject(False)
+        :return: nothing
+        """
+        if not status:
+            student.status = False
--- dtabases/courses/read_corses.py
+++ dtabases/courses/read_corses.py
@@ -1,7 +1,17 @@
 import csv
 
-courses_list = []
-with open('courses.csv', 'r', newline='') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
-    for row in csv_reader:
-        courses_list.append(row)
+def read_db():
+    courses_list = []
+    with open('courses.csv', 'r', newline='') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            courses_list.append(row)
+    return courses_list
+
+def write_db(name,units,total_quantity,teacher_name,course_code,field_code):
+    with open('courses_list.csv', 'a+', newline='') as csv_file:
+        fieldnames = ['name', 'units', 'total_quantity', 'teacher_name', 'course_code', 'field_code']
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writerow({'name': name ,'units': units,
+                         'total_quantity': total_quantity, 'teacher_name': teacher_name,
+                         'course_code': course_code, 'field_code':field_code})
