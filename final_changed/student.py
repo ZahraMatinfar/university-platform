3ddcf8f86e63cc145322bcdd71d168e0c47a73ce
LGhomi <leilaghomi73@gmail.com>
2020-11-22 16:36:39 +0330

    table in submit method of student added

--- final_changed/student.py
+++ final_changed/student.py
@@ -1,18 +1,17 @@
 import csv
-
-from prettytable import PrettyTable
+import json
 
 from classes.course import Course
 from classes.user import User
 from databases.course.read_courses import read_db
-
+from prettytable import PrettyTable
+import pandas as pd
 
 class Student(User):
     # initialize student attributes
     def __init__(self, username, password, user_id, first_name, last_name, user_type, field_name, field_code):
-        self.take_courses_status = True
+        self.take_courses_status = None
         self.total_units = 0
-        self.available_courses = []
         self.chosen_courses = []
         super().__init__(username, password, user_id, first_name, last_name, user_type, field_name, field_code)
 
@@ -54,25 +53,29 @@ class Student(User):
         :return: nothing
         """
 
+        available_courses = []
         courses_list = read_db()
         for course in courses_list:
             if self.field_code == int(course['field_code']) or int(course['field_code']) == 0:
-                self.available_courses.append(
+                available_courses.append(
                     Course(course['name'], int(course['units']), int(course['total_quantity']), course['teacher_name'],
                            int(course['course_code']), int(course['field_code'])))
+        return available_courses
 
     def show_available_courses(self):
         """
         print available courses for student when 'Offered courses in current semester' option  in menu selected
         :return:nothing
         """
-        t = PrettyTable()  # create table of available courses
-        t.clear_rows()
-        t = PrettyTable(['course_code', 'name', 'units', 'total_quantity', 'teacher_name', 'field_code'])
-        for course in self.available_courses:
-            t.add_row([course.course_code, course.name, course.units, course.total_quantity, course.teacher_name,
-                       course.field_code])
-        print(t)
+
+        table = PrettyTable(
+            ['course code', 'course name', 'units', 'teacher name', 'field code', 'total quantity'])
+
+        for lesson in self.defined_available_courses():
+            table.add_row(
+                [lesson.course_code, lesson.name, lesson.units, lesson.teacher_name, lesson.field_code,
+                 lesson.total_quantity])
+        print(table)
 
     def add_course(self, course_code):
         """
@@ -81,7 +84,7 @@ class Student(User):
         :return: True if course have enough quantity,else:False.None If course is chosen already.
         """
 
-        for course in self.available_courses:
+        for course in self.defined_available_courses():
 
             # self.available_courses contains list of Course objects
             if course_code == course.course_code:
@@ -117,8 +120,14 @@ class Student(User):
         if len(self.chosen_courses) == 0:
             print('No courses have been added yet.')
         else:
-            for course in self.chosen_courses:
-                print(course)
+            table = PrettyTable(
+                ['course code', 'course name', 'units', 'teacher name', 'field code', 'total quantity'])
+
+            for lesson in self.chosen_courses:
+                table.add_row(
+                    [lesson.course_code, lesson.name, lesson.units, lesson.teacher_name, lesson.field_code,
+                     lesson.total_quantity])
+            print(table)
 
     def submit(self):
         """
@@ -127,17 +136,14 @@ class Student(User):
         :return: True if number of units are between 10 and 20/else:False
         """
 
+
         if self.check_units() != -1 and self.check_units() != 1:
-            with open('students_info.csv', 'a', newline='') as csv_file:
-                write_student_info = csv.DictWriter(csv_file, fieldnames='student_id ')
-                write_student_info.writeheader()
-                field_names = ['name', 'course_code']
-                write_course_info = csv.DictWriter(csv_file, fieldnames=field_names)
-                write_course_info.writeheader()
-                write_student_info.writerow(
-                    {f'{self.user_id}': write_course_info.writerow(
-                        {'name': course.name, 'course_code': course.course_code}) for
-                        course in self.chosen_courses})
+            with open('../databases/users_db/students_info.json') as std_info:
+                json_data = json.load(std_info)
+                student_dict = {f'{self.user_id}': [vars(course) for course in self.chosen_courses]}
+            json_data.update(student_dict)
+            with open('../databases/users_db/students_info.json', 'w') as std_info:
+                json.dump(json_data, std_info)
             return True
         else:
             return False
@@ -147,8 +153,16 @@ class Student(User):
         after submit courses show final chosen courses depending on that admin approve or reject them
         :return:nothing
         """
-        submited_course = []
+
         if self.take_courses_status:
-            self.show_chosen_courses()
+            with open('../databases/users_db/students_info.json') as std_info:
+                info = json.load(std_info)
+                table = PrettyTable(['course code', 'course name', 'units', 'teacher name', 'field code', 'total quantity'])
+                for course in info[f'{self.user_id}']:
+                    values = []
+                    for i in ['course_code', 'name', 'units', 'teacher_name', 'field_code', 'total_quantity']:
+                        values.append(course[i])
+                    table.add_row(values)
+                print(table)
         else:
             print('your request has been rejected')
